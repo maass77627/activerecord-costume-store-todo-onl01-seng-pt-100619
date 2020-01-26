b889b5ea7b7ea5399847fa544fbc23994dbbbad7
kelly r maass <kmaass77627@gmail.com>
2020-01-26 19:02:33 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,10 +11,11 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
      create_table :costumes do |t|
-      t.string :name
-      t.string :price
-      t.integer :size
+     t.string :name
+      t.float :price
       t.string :image_url
+      t.string :size
+      t.timestamps
   end
   end
 end
