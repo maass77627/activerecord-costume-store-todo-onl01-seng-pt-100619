97100e765f9683212d37b505b25114ebda9c059f
kelly r maass <kmaass77627@gmail.com>
2020-01-26 19:08:22 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,11 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change 
     create_table :costumes do |t|
-    
+      t.string :name 
+      t.string :location
+      t.integer :number_of_costumes 
+      t.integer :number_of_employees
+      t.string 
     
   end 
   
